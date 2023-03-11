22c6a792a03cd4daf4903546c4b6975dfd8925c1
AryavirMalik07 <aryavirmalik20@gmail.com>
2023-03-11 13:35:16 +0530

    file add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-// import logo from './logo.svg';
-import "./App.css";
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Events from "./pages/Events";
+import Home from "./pages/Home";
+const App = () => {
   return (
-    <div className="App">
-      <h1> Hello</h1>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event" element={<Events />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/AboutUs.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../styles/About.css";
+const AboutUs = () => {
+  return <div>AboutUs</div>;
+};
+
+export default AboutUs;
